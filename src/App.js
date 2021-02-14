4751f057433eeb37ea57c1ba4c1c7d590e3d371c
Allistair <allistair.vilakazi@gmail.com>
2021-02-15 01:31:38 +0200

    refactor: linting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import GetUserDetail from './components/GetUserDetail';
-import ShowUsers from './components/ShowUsers';
-import GamePlay from './components/GamePlay';
-import { Container } from 'react-bootstrap';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import GetUserDetail from "./components/GetUserDetail";
+import ShowUsers from "./components/ShowUsers";
+import GamePlay from "./components/GamePlay";
+import { Container } from "react-bootstrap";
 import socketIOClient from "socket.io-client";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      endpoint: "http://127.0.0.1:4444",
+      endpoint: "http://localhost:4444",
       socket: null,
       isGameStarted: false,
-      gameId:null,
+      gameId: null,
       gameData: null,
     };
   }
@@ -24,8 +24,8 @@ class App extends Component {
     const { endpoint } = this.state;
     // Made a connection with server
     const socket = socketIOClient(endpoint);
-    socket.on("connected", data => {
-      this.setState({ socket: socket })
+    socket.on("connected", (data) => {
+      this.setState({ socket: socket });
     });
   }
   registrationConfirmation = (data) => {
@@ -34,7 +34,11 @@ class App extends Component {
   };
   gameStartConfirmation = (data) => {
     // If select opponent player then start game and redirect to game play
-    this.setState({ isGameStarted: data.status, gameId: data.game_id, gameData: data.game_data });
+    this.setState({
+      isGameStarted: data.status,
+      gameId: data.game_id,
+      gameData: data.game_data,
+    });
   };
   opponentLeft = (data) => {
     // If opponent left then get back from game play to player screen
@@ -44,16 +48,33 @@ class App extends Component {
   render() {
     return (
       <Container>
-        {
-          !this.state.isGameStarted ? !this.state.isRegistered ? <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            {this.state.socket
-              ? <GetUserDetail socket={this.state.socket} registrationConfirmation={this.registrationConfirmation} />
-              : <p>Loading...</p>}
-          </header> :
-            <ShowUsers socket={this.state.socket} gameStartConfirmation={this.gameStartConfirmation} /> :
-            <GamePlay socket={this.state.socket} gameId={this.state.gameId} gameData={this.state.gameData} opponentLeft={this.opponentLeft} />
-        }
+        {!this.state.isGameStarted ? (
+          !this.state.isRegistered ? (
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              {this.state.socket ? (
+                <GetUserDetail
+                  socket={this.state.socket}
+                  registrationConfirmation={this.registrationConfirmation}
+                />
+              ) : (
+                <p>Loading...</p>
+              )}
+            </header>
+          ) : (
+            <ShowUsers
+              socket={this.state.socket}
+              gameStartConfirmation={this.gameStartConfirmation}
+            />
+          )
+        ) : (
+          <GamePlay
+            socket={this.state.socket}
+            gameId={this.state.gameId}
+            gameData={this.state.gameData}
+            opponentLeft={this.opponentLeft}
+          />
+        )}
       </Container>
     );
   }
